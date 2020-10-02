a84f7bf0d9fe9d2229d3dac2fc4abf13af02676c
tlutsyshyn <taraslut78@gmail.com>
2020-10-02 20:32:48 +0300

    add unit tests

--- example.py
+++ example.py
@@ -1,2 +1,23 @@
 def hello(name):
-    return f"hello {name}"
+    # pass
+    return f"hello a"
+
+
+input = [
+    'a',
+    'Taras'
+]
+
+expected= [
+    'hello a',
+    'hello Taras'
+]
+
+def test():
+    for test_case, expect in zip(input, expected):
+        rez = hello(test_case)
+        assert rez == expect, f'hello(\'{test_case}\') is expected =>\'{expect}\' but \'{rez}\' is present'
+
+
+if __name__ == "__main__":
+    test()
